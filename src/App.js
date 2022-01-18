1b14db22657280bfe425febf829fda15d381283c
Tiago Medeiros <85853285+Tiagomedeirosds@users.noreply.github.com>
2022-01-18 15:11:34 -0500

    added className

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import MyListingPage from "./pages/MyListingPage/MyListingPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,49 +5,49 @@ import "./HomePage.css";
 export default function HomePage() {
   return (
     <div>
-        <div class="carouselContainer">
-            <div id="carouselExampleCaptions" class="carousel slide"       data-bs-ride="carousel">
-                  <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div className="carouselContainer">
+            <div id="carouselExampleCaptions" className="carousel slide"       data-bs-ride="carousel">
+                  <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                   </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                      <img src="/images/hero1.jpeg" class="d-block w-100" alt="..."/>
-                      <div class="carousel-caption ">
-                        <div class="textContainer">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                      <img src="/images/hero1.jpeg" className="d-block w-100" alt="..."/>
+                      <div className="carousel-caption ">
+                        <div className="textContainer">
                           <h5>Welcome to PurBnB</h5>
                           <p>Prove to your cat that you care!</p>
                         </div>  
                       </div>
                   </div>
-                  <div class="carousel-item">
-                    <img src="/images/hero2.jpeg" class="d-block w-100" alt="..."/>
-                    <div class="carousel-caption ">
-                    <div class="textContainer">
+                  <div className="carousel-item">
+                    <img src="/images/hero2.jpeg" className="d-block w-100" alt="..."/>
+                    <div className="carousel-caption ">
+                    <div className="textContainer">
                         <h5>For chill and very active cats!</h5>
                         <p>You can check with the host if there are enough beds or obstacles for you friend</p>
                       </div>
                     </div>
                   </div>
-                  <div class="carousel-item">
-                    <img src="/images/hero3.jpeg" class="d-block w-100" alt="..."/>
-                    <div class="carousel-caption ">
-                        <div class="textContainer">
+                  <div className="carousel-item">
+                    <img src="/images/hero3.jpeg" className="d-block w-100" alt="..."/>
+                    <div className="carousel-caption ">
+                        <div className="textContainer">
                           <h5>Go to your trip with no worries!</h5>
                           <p>Your cat will be in good hands</p>
                         </div>
                     </div>
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </div>
