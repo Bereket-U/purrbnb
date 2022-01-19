c732f929a6889266e9592b7391d7fb392a06dd57
Bereket Urgessa <bereketadugna15@gmail.com>
2022-01-18 21:12:05 -0500

    add NewListingForm component

---
+++ src/components/NewListingForm/NewListingForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NewListingForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New Listing Page</h1>
+      </div>
+    );
+  }
+}
--- src/pages/NewListingPage/NewListingPage.jsx
+++ src/pages/NewListingPage/NewListingPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NewListingForm from "../../components/NewListingForm/NewListingForm";
 
 export default function NewListingPage() {
   return (
     <div>
-      <h1>New Listing Page</h1>
+      <NewListingForm />
     </div>
   );
 }
