20f7b1b5621bcfbf9ba534d4f68d9744d788f2d9
Tiago Medeiros <85853285+Tiagomedeirosds@users.noreply.github.com>
2022-01-20 12:32:12 -0500

    fixing forms

--- src/components/NewListingForm/NewListingForm.jsx
+++ src/components/NewListingForm/NewListingForm.jsx
@@ -94,8 +94,8 @@ export default class NewListingForm extends Component {
   render() {
     return (
       <div>
-        {!this.props.editMode && <h1>New Listing Page</h1>}
-        <p className="">&nbsp;{this.state.message}</p>
+        {!this.props.editMode}
+        
         <div className="form-container">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <h1>New Listing</h1>
--- src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../../components/Card/Card";
 
+
 const SearchResultsPage = (props) => {
   const queryParams = new URLSearchParams(window.location.search);
   const term = queryParams.get("term");
