2175e4e5799c407b010ffb0f88eb0aaec9f433d1
Bereket Urgessa <bereketadugna15@gmail.com>
2022-02-14 13:13:03 -0500

    add footer for loged in users

--- src/App.js
+++ src/App.js
@@ -87,7 +87,6 @@ export default function App() {
               element={<AuthPage setUserInState={setUserInState} />}
             />
           </Routes>
-
           {/* <AuthPage setUserInState={this.setUserInState} /> */}
         </>
       )}
